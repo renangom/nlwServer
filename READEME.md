133cf913437bf12f302c5740d72e0e36129b73e2
Renan <renan23eduardo@gmail.com>
2022-09-15 14:45:33 -0300

    Create sqlite database with prisma

---
+++ READEME.md
@@ -0,0 +1,26 @@
+## Entidades
+
+### Game
+
+id
+title
+bannerUrl
+
+### Ad
+
+id
+gameId
+name
+yearsOfPlaying
+discord
+weekDays
+hourStart
+useVoiceChannel
+createdAt
+
+## Casos de uso
+
+- Listagem de games com contagem de anúncios
+- Criação de novo anúncio
+- Listagem de anúncios por game
+- Buscar discord pelo ID do anúncio
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,25 @@ import express, { Request, Response } from 'express'
 
 const app = express();
 
+//tipos de parâmetros
+// query --> vem através do ponto de interrogação ?page=2 --> usado para persistir estado
+// route --> são intrinsecamente conhecidos --> 
+// body
+
+
+app.get('/games', (req:Request, res:Response) => {
+    return res.json([]);
+})
+app.post('/ads', (req:Request, res:Response) => {
+    return res.status(201).json([]);
+})
+app.get('/games/:id/ads', (req:Request, res:Response) => {
+    return res.json([]);
+})
+app.get('/ads/:id/discord', (req:Request, res:Response) => {
+    return res.json([]);
+})
+
 app.get('/ads', (req:Request, res: Response) => {
     return res.json([
         {id: 1, name: 'Anúncio 1'},
@@ -11,4 +30,6 @@ app.get('/ads', (req:Request, res: Response) => {
     ])
 })
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('Server is running')
+});
